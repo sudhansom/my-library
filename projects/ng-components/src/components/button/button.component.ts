c6c1133a16bfb51a55638709c324b66d4889bfd5
Sandesh Shrestha <san@sitemule.com>
2022-07-21 17:53:02 +0200

    Add button component

---
+++ projects/ng-components/src/components/button/button.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostBinding, Input } from '@angular/core';
+
+
+type ButtonSize = 'sm' | 'md' | 'lg';
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: '[sitemule-button]',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() loading = false;
+  @Input() fullWidth = false;
+  @Input() stroked = false;
+  @Input() uppercase = false;
+  @Input() color = '';
+  @Input() size : ButtonSize = 'md';
+
+  @HostBinding('class') get HostClass() {
+    return `${this.color} ${this.loading !== false ? 'loading' : ''} ${
+      this.fullWidth !== false ? 'full-width' : ''
+    } ${this.stroked !== false ? 'stroked' : 'not-stroked'} size-${this.size} ${this.uppercase !== false ? 'uppercase' : ''}`;
+  }
+}
