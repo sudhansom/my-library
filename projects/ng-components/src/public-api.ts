992eb34a7189d2b2cb114fc302d51cc49480894c
Sandesh Shrestha <san@sitemule.com>
2022-09-14 16:42:13 +0200

    Export teaser-stack-2

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -4,3 +4,12 @@
 
 export * from './components/button/button.component';
 export * from './components/button/button.module';
+export * from "./components/teaser-stack/teaser-layout-1/teaser-layout-1.component";
+export * from "./components/teaser-stack/teaser-layout-2/teaser-layout-2.component";
+export * from "./components/teaser-stack/teaser-stack-layout-1/teaser-stack-layout-1.component";
+export * from "./components/teaser-stack/teaser-stack.component";
+export * from "./components/teaser-stack/teaser-stack.interface";
+export * from "./components/teaser-stack/teaser-stack.module";
+export * from "./components/teaser-stack/teaser/teaser.component";
+
+
