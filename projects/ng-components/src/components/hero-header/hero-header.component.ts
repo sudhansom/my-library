51d5d11d9e005baa1b99d74555d28da8764d44e0
bhuwan <bkspoudel@gmail.com>
2022-09-25 16:58:47 +0200

    add hero header to the library

---
+++ projects/ng-components/src/components/hero-header/hero-header.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+type TargetType = '_blank' | '_top';
+
+export interface ILink{
+  topLink?: boolean
+  label: string
+  href: string
+  target: string
+  backgroundColor: string
+  color: string
+}
+
+@Component({
+  selector: 'sm-hero-header',
+  templateUrl: './hero-header.component.html',
+  styleUrls: ['./hero-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeroHeaderComponent {
+  @Input() header: string = '';
+  @Input() description: string = '';
+  @Input() note: string = '';
+  @Input() links: ILink[] | undefined = [];
+  @Input() image: string = '';
+}
---
+++ projects/ng-components/src/components/hero-header/hero-header.stories.ts
@@ -0,0 +1,43 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { HeroHeaderComponent } from './hero-header.component';
+import { HeroHeaderModule } from './hero-header.module';
+
+
+export default {
+  title: 'Sitemule-Ui/HeroHeader',
+  component: HeroHeaderComponent,
+  parameters: {
+  },
+  decorators: [
+    moduleMetadata({
+      imports: [HeroHeaderModule],
+    }),
+  ],
+} as Meta;
+
+
+const headerLinks = [
+  {
+    label: 'Get Started',
+    href: 'https://www.folkelarsen.dk/page1',
+    target: '_blank',
+    backgroundColor: '#9925f1',
+    color: 'white',
+  }
+];
+
+const Template: Story<HeroHeaderComponent> = (args:HeroHeaderComponent) => ({
+  component:HeroHeaderComponent,
+  props: args,
+});
+
+export const heroHeader = Template.bind({});
+heroHeader.storyName = 'hero-header-layout1';
+heroHeader.args = {
+  header: 'Twenty five minutes from now',
+  description:
+    'You will build a web based customer service directly integrated to your IBM i™',
+  note: 'No BS required. Just download and use.',
+  links: headerLinks,
+  image: 'https://media.istockphoto.com/photos/copenhagen-denmark-at-nyhavn-canal-picture-id587892190?k=20&m=587892190&s=612x612&w=0&h=0GMRGE6VkLb61ryGz--XwoBD6ns0kwNWf-Dz9Agx_ew=',
+};
