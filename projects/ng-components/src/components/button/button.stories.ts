5f53b80482bc87b46871cf9874940f83e3fdd4ff
bhuwan <bkspoudel@gmail.com>
2022-09-26 15:44:44 +0200

    fix  mobileTabletSize in button story

--- projects/ng-components/src/components/button/button.stories.ts
+++ projects/ng-components/src/components/button/button.stories.ts
@@ -41,7 +41,7 @@ const AnchorTemplate: Story<ButtonComponent> = (args: ButtonComponent) => ({
       ${args.stroked ? 'stroked' : ''}
       ${args.uppercase ? 'uppercase' : ''}
       color="${args.color}"
-      mobileTabletSize="lg"
+      mobileTabletSize="${args.mobileTabletSize}"
       size="${args.size}">A href</a>`
 });
 export const AnchorButton = AnchorTemplate.bind({});
