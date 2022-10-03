7711bde066ecfc5e46a880782923c0dc0931e462
Sandesh Shrestha <san@sitemule.com>
2022-10-03 17:06:04 +0200

    Use scaleImage on Banner-layout-1

--- projects/ng-components/src/components/banner/banner-layout-1/banner-layout-1.module.ts
+++ projects/ng-components/src/components/banner/banner-layout-1/banner-layout-1.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavigationModule } from '../../../directives/navigation/navigation.module';
+import { ScaleImageDirectiveModule } from '../../../directives/scaleimage/scale-image.module';
 import { ButtonModule } from '../../button/button.module';
 import { BannerLayout1Component } from './banner-layout-1.component';
 
 @NgModule({
   declarations: [BannerLayout1Component],
-  imports: [NavigationModule, ButtonModule, CommonModule],
+  imports: [NavigationModule, ButtonModule, CommonModule, ScaleImageDirectiveModule],
   exports: [BannerLayout1Component],
   providers: [],
 })
