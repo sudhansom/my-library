db3c7ce132c342a575b429f2dcd635ca3167f2d9
bhuwan <bkspoudel@gmail.com>
2022-10-12 10:12:35 +0200

    remove subHeader and subDescription

--- projects/ng-components/src/components/article-component/article.component.ts
+++ projects/ng-components/src/components/article-component/article.component.ts
@@ -27,7 +27,5 @@ export class ArticleComponent {
   @Input() imageBorderRadius = '10px';
   @Input() backgroundColor: string = '';
   @Input() boxShadow: string = '';
-  @Input() subHeader: string = '';
-  @Input() subDescription: string = '';
   @Input() textColor: string = '';
 }
