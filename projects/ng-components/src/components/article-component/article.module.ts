c5dd2668950c246fff0ca8d1aa91f6ddc829f6de
bhuwan <bkspoudel@gmail.com>
2022-10-18 18:11:17 +0200

    remove markdown pipe from article section

--- projects/ng-components/src/components/article-component/article.module.ts
+++ projects/ng-components/src/components/article-component/article.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from '../button/button.module';
 
 import { ArticleComponent } from './article.component';
-import { MarkdownPipeModule } from '../../pipes/markdown/markdown.module';
 
 @NgModule({
   declarations: [ArticleComponent],
-  imports: [CommonModule, ButtonModule, MarkdownPipeModule],
+  imports: [CommonModule, ButtonModule],
   exports: [ArticleComponent],
   providers: [],
 })
